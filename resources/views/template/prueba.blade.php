a8c21e76bd3a3b7f051e8ea69ebe809121a4f919
msiAriel <ariel.taipe2640@utc.edu.ec>
2023-05-20 06:25:15 +0200

    changes

---
+++ resources/views/template/prueba.blade.php
@@ -0,0 +1,111 @@
+<div class="container">
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="page-content ">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="most-popular">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="heading-section">
+                                        <h4><em>Most Popular</em> Right Now</h4>
+                                    </div>
+                                    <div class="row">
+    
+                                        <div class="col-lg-12">
+                                            <div class="main-button">
+                                                <a href="browse.html">Discover Popular</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="most-popular">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="heading-section">
+                                        <h4><em>Most Popular</em> Right Now</h4>
+                                    </div>
+                                    <div class="row">
+    
+                                        <div class="col-lg-12">
+                                            <div class="main-button">
+                                                <a href="browse.html">Discover Popular</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="most-popular">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="heading-section">
+                                        <h4><em>Most Popular</em> Right Now</h4>
+                                    </div>
+                                    <div class="row">
+    
+                                        <div class="col-lg-12">
+                                            <div class="main-button">
+                                                <a href="browse.html">Discover Popular</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="most-popular">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="heading-section">
+                                        <h4><em>Most Popular</em> Right Now</h4>
+                                    </div>
+                                    <div class="row">
+    
+                                        <div class="col-lg-12">
+                                            <div class="main-button">
+                                                <a href="browse.html">Discover Popular</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="page-content ">
+                <!-- ***** Most Popular Start ***** -->
+                <div class="most-popular">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="heading-section">
+                                <h3>Noticias</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="most-popular">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="heading-section">
+                                <h3>Noticias</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+     return view('template.index');
+    // return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
