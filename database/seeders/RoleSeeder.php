54a6170068d13d8366115406606bb1a31464a77e
Jeimy Jara Desarrollador 1 <desarrollador1@imporfactoryusa.com>
2024-01-16 12:08:32 -0500

    local

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -12,10 +12,10 @@ class RoleSeeder extends Seeder
 
     public function run()
     {
-        // $admin = Role::create(['name' => 'Admin']);
-        // $client = Role::create(['name' => 'Client']);
-        // $especialista = Role::create(['name' => 'Especialista']);
-        // $alumno = Role::create(['name' => 'Alumno']);
+        $admin = Role::create(['name' => 'Admin']);
+        $client = Role::create(['name' => 'Client']);
+        $especialista = Role::create(['name' => 'Especialista']);
+        $alumno = Role::create(['name' => 'Alumno']);
 
         Permission::create(['name' => 'admin.dashboard', 'description' => 'Administrar sistema general'])->syncRoles(['Admin']);
         Permission::create(['name' => 'admin.users.index', 'description' => 'Administrar modulo usuarios'])->syncRoles(['Admin']);
