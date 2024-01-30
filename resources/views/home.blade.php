4375d0f259785d367bb2eeb8bad5b99d652c670e
Jeimy Jara Desarrollador 1 <desarrollador1@imporfactoryusa.com>
2024-01-30 14:21:18 -0500

    Fixed2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
+@php
+use Illuminate\Support\Str;
+@endphp
+
     <div class="container py-2">
         <div class="row justify-content-center">
             <div class="col-12 col-md-12 col-lg-6">
@@ -67,6 +71,7 @@
                             <div class="container-fluid d-flex align-items-center justify-content-center">
                                 <div class="d-flex justify-content-center">
                                     <div class="row text-center">
+                                        @can("acceso-sistema",10)
                                         <img style="width: 315px;" src="{{ asset('iconos/6.png') }}" alt="logo4">
                                             <a type="button" href="{{auth()->user()->url}}" target="_blank" class="btn">
                                                 <span class="m-1 letter-spacing" style="white-space: nowrap;">TIENDA <i
@@ -76,7 +81,16 @@
                                                 <span class="m-1 letter-spacing" style="white-space: nowrap;">TIENDA <i
                                                         class="fa fa-arrow-right ms-1"></i></span>
                                             </a> --}}
-        
+                                        
+                                        @else
+                                        <img style="width: 315px;  filter: grayscale(100%);" src="{{ asset('iconos/6.png') }}" alt="logo4">
+
+                                                <a type="button" href="#" class="btn">
+                                                    <span class="m-1 letter-spacing"
+                                                        style="white-space: nowrap;">NO DISPONIBLE</span>
+                                                </a>
+                                        
+                                        @endcan
                                     </div>
                                 </div>
                             </div>
@@ -116,13 +130,18 @@
                                     <div class="row text-center">
                                         
                                         <img style="width: 315px;" src="{{ asset('iconos/7.png') }}" alt="logo4">
-
-                                            <a type="button" href="{{auth()->user()->url.'/sysadmin/login.php'}}" target="_blank" class="btn"
-                                                disabled>
-                                                <span class="m-1 letter-spacing" style="">CONSTRUCTOR <i
-                                                        class="fa fa-arrow-right ms-1"></i></span>
-                                            </a>
-                                            {{-- <a type="button" href="http://108.181.168.234/importshop/sysadmin" class="btn"
+                                        @if(auth()->user()->url)
+                                        @php
+                                        $userUrl = auth()->user()->url;
+                                        $finalUrl = Str::contains($userUrl, 'registro.imporsuit') ? $userUrl : $userUrl.'/sysadmin/login.php';
+                                        @endphp
+                                        <a type="button" href="{{ $finalUrl }}" target="_blank" class="btn">
+                                            <!-- Tu texto o ícono aquí -->
+                                            <span class="m-1 letter-spacing" style="">CONSTRUCTOR <i
+                                                    class="fa fa-arrow-right ms-1"></i></span>
+                                        </a>
+                                    @endif
+                                                                                   {{-- <a type="button" href="http://108.181.168.234/importshop/sysadmin" class="btn"
                                                 disabled>
                                                 <span class="m-1 letter-spacing" style="">FACTURACIÓN ELECTRONICA <i
                                                         class="fa fa-arrow-right ms-1"></i></span>
@@ -169,14 +188,51 @@
                 </div>
             </div>
         </div>
-        @if ($suscripcion != '0')
+        @if ($suscripcion != '0' && $suscripcion->dias > 0)
         <div class="alert alert-success">
             {{ $suscripcion->nombre }}
             <span>Le quedan {{ $suscripcion->dias }} días para que su suscripción termine.</span>
         </div>
     @endif
+        @if($suscripcion != '0' && $suscripcion->dias <= 0)
+            <div class="alert alert-danger ">
+                <span>¿Observa que algunas herramientas aparecen en gris y están marcadas como 'no disponibles'?
+                    <button class="btn btn-sm btn-warning text-white" data-bs-toggle="modal" data-bs-target="#subscriptionExpiredModal">Averiguar ¿Por qué?</button>
+                </span>
+        </div>
+        
+        @endif
     </div>
-    
+    <!-- Modal -->
+<div class="modal fade" id="subscriptionExpiredModal" tabindex="-1" aria-labelledby="subscriptionExpiredModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+ 
+          
+          <div class="modal-body">
+            <h5 class="modal-title text-center fw-bold" id="subscriptionExpiredModalLabel">Suscripción Caducada</h5>
+            <hr>
+                <span class="">
+                Estimado usuario, <br>
+                
+                Hemos notado que su suscripción a nuestros servicios ha llegado a su término. <br>
+                
+                Durante el período de su suscripción, esperamos que haya disfrutado de las ventajas y beneficios exclusivos que ofrecemos. Su acceso a herramientas premium, contenido especializado y soporte prioritario ha sido suspendido temporalmente debido a la caducidad de su suscripción. <br>
+                
+                En ImporFactory, estamos comprometidos a ofrecer la mejor experiencia y valor a nuestros suscriptores. Renovar su suscripción no solo le permitirá recuperar el acceso inmediato a todas las herramientas y servicios que ha estado disfrutando, sino que también asegurará que continúe recibiendo las últimas actualizaciones, mejoras de seguridad y soporte exclusivo que vienen con su membresía. <br>
+                
+                Le invitamos cordialmente a renovar su suscripción para restablecer su acceso completo y continuar beneficiándose de las características y servicios exclusivos que ofrecemos. Si tiene alguna pregunta o necesita asistencia adicional con el proceso de renovación, no dude en contactarnos. <br>
+                
+                Apreciamos su comprensión y esperamos continuar teniéndolo como parte de nuestra comunidad.
+            </span>
+         
+        </div>
+        <a href="https://wa.link/11dgle" class="btn btn-primary" target="_blank">Renovar Ahora</a>
+        
+      </div>
+    </div>
+  </div>
+  
     <style>
         .letter-spacing {
             letter-spacing: 0.05em;
